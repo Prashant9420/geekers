8a6043f113f30d02d388104ee92e9b542203b3d0
ankitgupta-13 <guptankit0522@gmail.com>
2023-03-22 18:23:17 +0530

    Changed Server URL

--- Frontend/src/utils/ServerURL.js
+++ Frontend/src/utils/ServerURL.js
@@ -1,4 +1,4 @@
-// const ServerURL = "https://geekers-api.onrender.com/api";
-const ServerURL = "http://localhost:8000/api";
+const ServerURL = "https://geekers-api.onrender.com/api";
+// const ServerURL = "http://localhost:8000/api";
 
 export default ServerURL;
