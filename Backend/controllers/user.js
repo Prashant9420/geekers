c3f425b79423653d8473eae815a370dc671a5551
ankitgupta-13 <guptankit0522@gmail.com>
2023-04-02 17:03:21 +0530

    +1

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -16,7 +16,7 @@ export const connectPassport = () => {
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         // callbackURL: "http://localhost:8000/api/user/googleLogin/callback",
         callbackURL:
-          "https://geekers-production.up.railway.app/api/user/googleLogin/callback",
+          "https://adventurous-volleyball-production.up.railway.app/api/user/googleLogin/callback",
       },
       async function (accessToken, refreshToken, profile, done) {
         // database logic
--- Frontend/src/utils/ServerURL.js
+++ Frontend/src/utils/ServerURL.js
@@ -1,5 +1,6 @@
 // const ServerURL = "http://geekers.onrender.com/api";
 // const ServerURL = "http://localhost:8000/api";
-const ServerURL = "https://geekers-production.up.railway.app/api";
+const ServerURL =
+  "https://adventurous-volleyball-production.up.railway.app/api";
 
 export default ServerURL;
